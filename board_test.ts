7ba5bd695192ca32dfc46f8892b62a18c2423a33
HeiligerGG <gggigi111@gmail.com>
2025-12-17 11:14:58 +0100

    Add Failing testes for red to green fixes

--- board_test.ts
+++ board_test.ts
@@ -6,3 +6,38 @@ Deno.test("dummy", () => {
   const winner = board.winner(Player.PlayerX, 1, 1);
   assertEquals(winner, Player.Nobody);
 });
+
+Deno.test("diagonaler Sieg fallend", () => {
+  const board = new Board();
+  
+  board.makeMove(Player.PlayerO, 0);
+  board.makeMove(Player.PlayerO, 0);
+  board.makeMove(Player.PlayerO, 0);
+  board.makeMove(Player.PlayerX, 0);
+  
+  board.makeMove(Player.PlayerO, 1);
+  board.makeMove(Player.PlayerO, 1);
+  board.makeMove(Player.PlayerX, 1);
+  
+  board.makeMove(Player.PlayerO, 2);
+  board.makeMove(Player.PlayerX, 2);
+  
+  const row = board.makeMove(Player.PlayerX, 3);
+  
+  const winner = board.winner(Player.PlayerX, row, 3);
+  assertEquals(winner, Player.PlayerX);
+});
+
+Deno.test("volle Spalte gibt -1 zurueck", () => {
+  const board = new Board();
+  
+  board.makeMove(Player.PlayerX, 0);
+  board.makeMove(Player.PlayerO, 0);
+  board.makeMove(Player.PlayerX, 0);
+  board.makeMove(Player.PlayerO, 0);
+  board.makeMove(Player.PlayerX, 0);
+  board.makeMove(Player.PlayerO, 0);
+  
+  const row = board.makeMove(Player.PlayerX, 0);
+  assertEquals(row, -1);
+});
